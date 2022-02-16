9b0e0249fa2c5eb8279a164a61560a61c4940180
CharlesGaydon <charles.gaydon@gmail.com>
2022-02-16 18:45:05 +0100

    Update env definition - more flexibility for python version

--- bash/setup_environment/setup_env.sh
+++ bash/setup_environment/setup_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
-conda install mamba -n base -c conda-forge
+pip install --upgrade pip
+
+conda install mamba --yes -n base -c conda-forge # mamba is a conda on steroids
 mamba env create -f bash/setup_environment/requirements.yml
 conda activate lidar_prod
