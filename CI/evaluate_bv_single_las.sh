5f7cd1cfd85b5def3fa1f809b95302b03d06ab11
CharlesGaydon <charles.gaydon@gmail.com>
2022-02-18 11:28:49 +0100

    Specify pickled thresholds as optimization param for CI

--- CI/evaluate_bv_single_las.sh
+++ CI/evaluate_bv_single_las.sh
@@ -13,4 +13,4 @@ print_config=true \
 building_validation.optimization.todo='prepare+evaluate+update' \
 building_validation.optimization.paths.input_las_dir=/var/data/cicd/CICD_github_assets/M8.0/20220204_building_val_V0.0_model/20211001_buiding_val_val/ \
 building_validation.optimization.paths.results_output_dir=/var/data/cicd/CICD_outputs/opti/ \
-building_validation.application.paths.building_validation_thresholds_pickle=/var/data/cicd/CICD_github_assets/M8.0/20220204_building_val_V0.0_model/M8.0B2V0.0_buildingvalidation_thresholds.pickle
+building_validation.optimization.paths.building_validation_thresholds_pickle=/var/data/cicd/CICD_github_assets/M8.0/20220204_building_val_V0.0_model/M8.0B2V0.0_buildingvalidation_thresholds.pickle
