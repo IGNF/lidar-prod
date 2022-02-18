89ee458f482c973663fcdf939abd3efd8228336d
Michel Daab <michel.daab@ign.fr>
2022-02-18 11:55:46 +0100

    linter temp

--- bash/setup_environment/setup_env_no_gpu.sh
+++ bash/setup_environment/setup_env_no_gpu.sh
@@ -18,10 +18,11 @@ pip install rich==11.2.0
 pip install pytest==6.2.5
 pip install optuna==2.10.0
 pip install pyshp==2.2.0
+pip install flake8==4.0.1
 
 conda install -y -c conda-forge pdal==2.3.0
 mamba install -y pytorch==1.8.1 -c pytorch -c conda-forge
-mamba install -y pyg==2.0.3 -c pytorch -c pyg -c conda-forge	# update cudatoolkit à 11.3.1 et pytorch à 1.10.2
+mamba install -y pyg==2.0.3 -c pytorch -c pyg -c conda-forge	# update cudatoolkit to 11.3.1 et pytorch to 1.10.2
 mamba install -y pytorch-lightning==1.5.8 -c conda-forge
-FORCE_CUDA=0 pip install torch-points-kernels --no-cache	# is angry because no daal==2021.3.0, but works nonetheless
+FORCE_CUDA=0 pip install torch-points-kernels --no-cache	# is angry because no daal==2021.3.0, but pip install flake8==4.0.1 works nonetheless
 pip install torchmetrics==0.6.2
