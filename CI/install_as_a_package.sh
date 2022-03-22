cdd29619630b234bbb4edeb33c8055d38b08d4ba
CharlesGaydon <charles.gaydon@gmail.com>
2022-03-22 12:32:56 +0100

    Remove unused CI scripts that are now in workflows and dockerfile

--- CI/install_as_a_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate lidar_prod
-HYDRA_FULL_ERROR=1
-
-pip install --no-cache-dir -e .  # install lidar_prod as a package
-python -m lidar_prod.run -h  # dry run to test install
