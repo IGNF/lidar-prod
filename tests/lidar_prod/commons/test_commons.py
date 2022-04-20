6af76c0ac82cfd5eb53be3e9488b03454ec9b50d
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-20 16:11:17 +0200

    Unit test for eval_time decorator

--- tests/lidar_prod/commons/test_commons.py
+++ tests/lidar_prod/commons/test_commons.py
@@ -1,15 +1,16 @@
+import logging
 import time
 
 from lidar_prod.commons.commons import eval_time
 
 
 def test_eval_time(caplog):
+    caplog.set_level(logging.INFO)
     durations = [0.1,0.2]
     @eval_time
     def sleeper(d):
         time.sleep(d)
     for d in durations:
         sleeper(d)
-        for record in caplog.records:
-            print(record)
+        assert caplog.records[-1].message.endswith(f"{d}s")
     
