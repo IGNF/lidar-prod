5b11ff252378cbe89379dc8e3d77c4b0f7480c38
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-20 16:17:05 +0200

    Add referenc url

--- tests/lidar_prod/commons/test_commons.py
+++ tests/lidar_prod/commons/test_commons.py
@@ -3,13 +3,15 @@ import time
 
 from lidar_prod.commons.commons import eval_time
 
-
+# See https://docs.pytest.org/en/latest/how-to/logging.html#caplog-fixture
 def test_eval_time(caplog):
     caplog.set_level(logging.INFO)
     durations = [0.1,0.2]
+
     @eval_time
     def sleeper(d):
         time.sleep(d)
+
     for d in durations:
         sleeper(d)
         assert caplog.records[-1].message.endswith(f"{d}s")
