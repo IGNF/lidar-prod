98647bf1d8eee4254c5402b99711a9afb8743bfa
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-22 14:35:46 +0200

    Refactor test of application for parameterization

--- tests/lidar_prod/test_application.py
+++ tests/lidar_prod/test_application.py
@@ -10,46 +10,56 @@ from tests.conftest import (
     pdal_read_las_array,
 )
 
-# test a full run with no error
 
 IN_F = "tests/files/870000_6618000.subset.postIA.las"
 
+# TODO: apply on 870000_6618000.subset.postIA
+# Raw, and test output for invariance and classification codes.
+# After transforming its channel :
+# building = 0 everywhere
+# Classification = 0 everywhere
+# Classification = candidate code everywhere
+# With another file of country area without any building <50m.
+
 
 def test_apply_on_subset(default_hydra_cfg):
+    # Expected classification codes after application run.
+    _fc = default_hydra_cfg.data_format.codes.building.final
+    expected_codes = {
+        1,
+        2,
+        _fc.building,
+        _fc.not_building,
+        _fc.unsure,
+    }
+    # Run application on the input data
     default_hydra_cfg.paths.src_las = IN_F
-    # TODO: ignore the warning here.
-    with tempfile.TemporaryDirectory() as td:
-        default_hydra_cfg.paths.output_dir = td
+    with tempfile.TemporaryDirectory() as default_hydra_cfg.paths.output_dir:
         out_f = apply(default_hydra_cfg)
-
-        # Check that key dimensions are unchanged
         assert_las_invariance(IN_F, out_f)
+        assert_format_of_application_output_las(out_f, expected_codes)
 
-        # Check that we contain extra_dims that production needs
-        assert_las_contains_dims(out_f, dims_to_check=["Group", "building", "entropy"])
 
-        # Ensure that the data format is the right one.
-        metadata = get_las_metadata(out_f)
-        assert metadata["minor_version"] == 4
-        assert metadata["dataformat_id"] == 8
+def assert_format_of_application_output_las(out_f: str, expected_codes: dict):
+    """Check LAS format, dimensions, and classification codes of output
 
-        # Ensure that the final spatial reference is French CRS Lambert-93
-        assert "Lambert-93" in metadata["spatialreference"]
+    Args:
+        out_f (str): path of output LAS
+        expected_codes (dict): expected classification codes.
 
-        arr1 = pdal_read_las_array(out_f)
+    """
+    # Check that we contain extra_dims that production needs
+    assert_las_contains_dims(out_f, dims_to_check=["Group", "building", "entropy"])
 
-        # Check that we have 1 group of suggested building points
-        assert arr1["Group"].min() == 0
-        assert arr1["Group"].max() == 1
+    # Ensure that the format versions are as expected
+    metadata = get_las_metadata(out_f)
+    assert metadata["minor_version"] == 4
+    assert metadata["dataformat_id"] == 8
+    # Ensure that the final spatial reference is French CRS Lambert-93
+    assert "Lambert-93" in metadata["spatialreference"]
 
-        # Check that we have either 1/2 (ground/unclassified), or one of the three final classification code of the module
-        final_codes = default_hydra_cfg.data_format.codes.building.final
-        expected_codes = {
-            1,
-            2,
-            final_codes.building,
-            final_codes.not_building,
-            final_codes.unsure,
-        }
-        actual_codes = {*np.unique(arr1["Classification"])}
-        assert expected_codes == actual_codes
+    # Check that we have either 1/2 (ground/unclassified),
+    # or one of the three final classification code of the module
+    arr1 = pdal_read_las_array(out_f)
+    actual_codes = {*np.unique(arr1["Classification"])}
+    assert actual_codes.issubset(expected_codes)
