b79ca007e9c1cd34e88f968e55db40c16e6c927d
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-25 09:35:12 +0200

    Document optimization tests

--- tests/lidar_prod/test_optimization.py
+++ tests/lidar_prod/test_optimization.py
@@ -1,11 +1,10 @@
+import pytest
 import os
+import os.path as osp
+import tempfile
 import hydra
 import numpy as np
 
-import os.path as osp
-import tempfile
-
-import pytest
 
 from lidar_prod.tasks.building_validation_optimization import (
     BuildingValidationOptimizer,
@@ -16,10 +15,15 @@ from tests.conftest import (
     pdal_read_las_array,
 )
 
-"""We test the building validation optimizer against
+"""We test the building validation optimizer against two LAS:
+
+These datasets must have the right classification codes, i.e. the ones defined in labels_from_20211001_building_val.
+The large LAS cannot be versionned by git. If it is absent from environment, the test is expected to fail.
+
 """
 
-
+# Small LAS, for which we optimize thresholds and reach perfect validation, 
+# to quickly check optimization logic. 
 IN_F = "tests/files/870000_6618000.subset.postIA.corrected.las"
 IN_F_EXPECTED = {
     "exact": {
@@ -32,6 +36,8 @@ IN_F_EXPECTED = {
         "precision": 1.0,
     },
 }
+# Large LAS, for which we evaluate performance, to control that there was no regression in terms of
+# automation/precision/recall of building validation.
 IN_F_LARGE = "tests/files/V0.5_792000_6272000.las"
 IN_F_LARGE_EXPECTED = {
     "exact": {
@@ -46,8 +52,9 @@ IN_F_LARGE_EXPECTED = {
     },
 }
 
-# Metric m must be above 1-0.01 = 99% times the expected value.
-RELATIVE_MIN_TOLERANCE_FOR_MIN_EXPECTED_METRICS = 0.01
+# Relative tolerance when comparing metrics to their expected value for large LAS.
+# i.e. resulting metrics are >= (1-tolerance) * expected metrics for performance indicators.
+RELATIVE_MIN_TOLERANCE_OF_EXPECTED_METRICS = 0.01
 
 
 def test_BVOptimization_on_subset(default_hydra_cfg):
@@ -82,7 +89,7 @@ def test_BVOptimization_on_subset(default_hydra_cfg):
         assert IN_F_EXPECTED["exact"].items() <= metrics_dict.items()
         for k, v in IN_F_EXPECTED["min"].items():
             assert (
-                (1 - RELATIVE_MIN_TOLERANCE_FOR_MIN_EXPECTED_METRICS) * v
+                (1 - RELATIVE_MIN_TOLERANCE_OF_EXPECTED_METRICS) * v
             ) <= metrics_dict[k]
         # Update with final codes and check if they are the right ones.
         bvo.bv.use_final_classification_codes = True
@@ -123,8 +130,6 @@ def test_BVOptimization_on_large_file(default_hydra_cfg):
         pipeline = get_a_copy_pdal_pipeline(IN_F_LARGE, in_f_copy)
         pipeline.execute()
 
-        # Be sure that we use default thresholds and not previous ones
-        default_hydra_cfg.building_validation.optimization
         # Check that a full optimization run can pass successfully
         bvo: BuildingValidationOptimizer = hydra.utils.instantiate(
             default_hydra_cfg.building_validation.optimization
@@ -134,35 +139,34 @@ def test_BVOptimization_on_large_file(default_hydra_cfg):
 
         exact_expected_val = IN_F_LARGE_EXPECTED["exact"]
         for k in exact_expected_val:
-            assert pytest.approx(exact_expected_val[k], 0.01) == metrics_dict[k]
+            assert pytest.approx(exact_expected_val[k], RELATIVE_MIN_TOLERANCE_OF_EXPECTED_METRICS) == metrics_dict[k]
         min_expected_val = IN_F_LARGE_EXPECTED["min"]
         for k in min_expected_val:
             assert (
-                (1 - RELATIVE_MIN_TOLERANCE_FOR_MIN_EXPECTED_METRICS)
-                * min_expected_val[k]
+                (1 - RELATIVE_MIN_TOLERANCE_OF_EXPECTED_METRICS) * min_expected_val[k]
             ) <= metrics_dict[k]
 
 
-# Expected metrics for reference until test is stable.
-# """
-# groups_count=1493
-# group_unsure=0.00402
-# group_no_buildings=0.149
-# group_building=0.847
-# p_auto=0.889
-# p_unsure=0.111
-# p_refute=0.0924
-# p_confirm=0.797
-# a_refute=0.899
-# a_confirm=0.976
-# precision=0.98
-# recall=0.99
-# Confusion Matrix
-# [[   2    1    3]
-# [  74  124   25]
-# [  89   13 1162]]
-# Confusion Matrix (normalized)
-# [[0.333 0.167 0.5  ]
-# [0.332 0.556 0.112]
-# [0.07  0.01  0.919]]
-# """
+# All expected metrics for reference:
+"""
+    groups_count=1493
+    group_unsure=0.00402
+    group_no_buildings=0.149
+    group_building=0.847
+    p_auto=0.889
+    p_unsure=0.111
+    p_refute=0.0924
+    p_confirm=0.797
+    a_refute=0.899
+    a_confirm=0.976
+    precision=0.98
+    recall=0.99
+    Confusion Matrix
+    [[   2    1    3]
+    [  74  124   25]
+    [  89   13 1162]]
+    Confusion Matrix (normalized)
+    [[0.333 0.167 0.5  ]
+    [0.332 0.556 0.112]
+    [0.07  0.01  0.919]]
+"""
