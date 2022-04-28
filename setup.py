91993629b5e8c9332286910582828c2ff49a612a
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-28 16:01:10 +0200

    Version bump to 1.5.0

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="lidar_prod",
-    version="1.4.1",
+    version="1.5.0",
     description="A 3D semantic segmentation production tool to augment rules-based Lidar classification with AI and databases.",
     author="Charles GAYDON",
     author_email="charles.gaydon@gmail.com",
-    # replace with your own github project link
     url="https://github.com/IGNF/lidar-prod-quality-control",
     install_requires=[],  # env should match the one in bash/setup_environment/setup_env.sh
     packages=find_packages(),
