c43551b03ef2fc7f4b956359cdc35e05e7a78021
Michel Daab <michel.daab@ign.fr>
2022-08-24 14:45:50 +0200

    Change to CICD to test the building detection

--- lidar_prod/run.py
+++ lidar_prod/run.py
@@ -11,6 +11,7 @@ class POSSIBLE_TASK(Enum):
     ID_VEGETATION_UNCLASSIFIED = "identify_vegetation_unclassified"
     OPT_VEGETATION = "optimize_veg_id"
     OPT_UNCLASSIFIED = "optimize_unc_id"
+    APPLY_BUILDING = "apply_on_building"
     OPT_BUIlDING = "optimize_building"
 
 
@@ -25,7 +26,7 @@ def main(config: DictConfig):  # pragma: no cover
     # Imports should be nested inside @hydra.main to optimize tab completion
     # Read more here: https://github.com/facebookresearch/hydra/issues/934
     from lidar_prod.commons.commons import extras
-    from lidar_prod.application import apply, identify_vegetation_unclassified, just_clean
+    from lidar_prod.application import apply, identify_vegetation_unclassified, just_clean, apply_building_module
     from lidar_prod.optimization import optimize_building, optimize_vegetation, optimize_unclassified
 
     log = logging.getLogger(__name__)
@@ -46,12 +47,15 @@ def main(config: DictConfig):  # pragma: no cover
     elif config.get("task") == POSSIBLE_TASK.CLEANING.value:
         apply(config, just_clean)
 
+    elif config.get("task") == POSSIBLE_TASK.APPLY_BUILDING.value:
+        apply(config, apply_building_module)
+
     elif config.get("task") == POSSIBLE_TASK.OPT_BUIlDING.value:
         optimize_building(config)
 
     else:
         log.info("Starting applying the default process")
-        optimize_building(config)
+        apply(config, apply_building_module)
 
 
 if __name__ == "__main__":  # pragma: no cover
