683f0ffa550d8a7a56e4dbd47e0d1246a6e16605
CharlesGaydon <charles.gaydon@gmail.com>
2023-01-19 15:37:46 +0100

    Document troubleshooting of mamba related error when installing new v of pdal

--- setup_env/setup_env.sh
+++ setup_env/setup_env.sh
@@ -6,9 +6,15 @@
 # Run this from a bash using
 # source setup_env/setup_env.sh
 
+# Note:
 
 set -e
 
-conda install mamba --yes -n base -c conda-forge # mamba is a conda on steroids
+conda install mamba --yes -n base -c conda-forge  # mamba is a conda on steroids
 mamba env create -f setup_env/requirements.yml --force
 conda activate lidar_prod
+
+# Troubleshooting:
+# Problem: nothing provides __glibc >=2.17,<3.0.a0 needed by libgdal-3.6.2-h10cbb15_3
+# If you encounter this error,
+# Creating the environment via conda instead of mamba might fix it, depending of your system.
