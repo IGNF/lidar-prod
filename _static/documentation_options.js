df9d94c29259fb65cbbdb257e0e154c235428da2
MichelDaab <93936641+MichelDaab@users.noreply.github.com>
2023-02-27 09:34:23 +0000

    Deploying to gh-pages from  @ 9f45c4a4f015778fb99049ad0f22e269eddcc809 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V1.9.9',
+    VERSION: 'V1.9.10',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
