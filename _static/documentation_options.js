43e46c4291ad59408bd9d01ebc0bfa87db3b0db5
gliegard <guillaume.liegard@gmail.com>
2023-06-21 08:40:04 +0000

    Deploying to gh-pages from  @ 536204a247c230476e11cd96b71defc391f51ba6 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V1.9.10',
+    VERSION: 'V1.9.11',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
