e85bb80415aa14302f65e64b438e2ecf74e747d7
CharlesGaydon <11660435+CharlesGaydon@users.noreply.github.com>
2023-10-17 14:32:17 +0000

    Deploying to gh-pages from  @ c8b59b99df86bd004c67f99ba1ccfa70149e3733 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V1.9.12',
+    VERSION: 'V1.9.13',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
